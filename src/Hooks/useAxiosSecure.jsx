e1c39bdcdd9cea78d3b4a57e79ff45bd26962740
Owasiul <sanisuper0@gmail.com>
2026-01-25 23:49:22 +0600

    added user validation

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const instance = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
+  const { user, LogOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // intercept request
+    const reqInterceptor = instance.interceptors.request.use(async (config) => {
+      if (user) {
+        const token = await user?.getIdToken();
+        config.headers.authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    // intercept response
+    const resInterceptor = instance.interceptors.response.use(
+      async (response) => {
+        return response;
+      },
+      (error) => {
+        const statusCode = error.response?.status;
+        if (statusCode === 401 || statusCode === 403) {
+          LogOut().then(() => {
+            navigate("/auth/login");
+          });
+        }
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      instance.interceptors.request.eject(reqInterceptor);
+      instance.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, LogOut, navigate]);
+
   return instance;
 };
 
